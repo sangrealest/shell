f23353e43e101bd9ce82adaf3c99a6d99b060d16
Shanker <shankerlee@gmail.com>
2016-03-04 15:38:29 +0800

    update mongoinstall

--- mongoDB_Install.sh
+++ mongoDB_Install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-#source ./configuration.conf
-#Mongo Server Info
 SlaveIP='10.128.129.45'
-SlaveName='Database-Slave'
+SlaveName='Databse-Slave'
 SlaveMongoPort='27017'
 
 ArbiterIP='10.128.129.46'
@@ -14,106 +12,100 @@ MasterIP='10.128.129.44'
 MasterName='Database-Master'
 MasterMongoPort='27017'
 
-
-#Mongo s config
+#Mongos config
 MongosPort='27015'
 
-
-#Mongo Config Server Conffig
+#Mongo COnfig Server Config
 MongoConfigIP='127.0.0.1'
 MongoConfigName='Database-Config'
 MongoConfigDBPath='/var/lib/mongodc'
 MongoConfiglogpath='/var/log/mongodc'
 MongoConfigPort='27014'
-MongoConfigkeyFile = '/etc/mongodb.key'
+#MongoConfigkeyFile = '/etc/mongodb.key'
 
 
-
-
-function CreateConfig(){
-    echo " Create Mongo config file................."
-    rolename=$1
-    mongoConfig="./monggod-$rolename"
-    if [ ! -f "$mongoConfig" ]
-
-    echo "dbpath=/var/lib/mongodb-$rolename" >> $mongoConfig
-    echo "logpath=/var/log/mongodb-$character/mongodb.log" >> $mongoConfig
-    echo "logappend=true" >> $mongoConfig
-    if [ "$rolename" == "master" ]
+function createConfig(){
+    echo "Creating Mongo config file"
+    role=$1
+    mongoConfig="./mongod-$role"
+    if [ ! -f "$mongoConfig ]
     then
-        echo "port = $MasterMongoPort" >> $mongoConfig
-    elif [ "$rolename" == "slave" ]
-    then
-        echo "port = $SlaveMongoPort" >> $mongoConfig
-    elif [ "$rolename" == "arbiter" ]
-    then
-        echo "port = $ArbiterMongoPort" >> $mongoConfig
+        echo "dbpath=/var/lib/mongodb-$role" >>$mongoConfig
+        echo "logpath=/var/log/mongodb-$role/mongodb.log" >>$mongoConfig
+        echo "logappend=true" >>$mongoConfig
+        if [ "$role" == "master" ]
+        then
+            echo "port = $MasterMongoPort" >>$mongoConfig
+        elif [ "$role" == "slave" ]
+        then
+            echo "port = $SlaveMongoPort" >>$mongoConfig
+        elif [ "$role" == "arbiter" ]
+        then
+            echo "port = $ArbiterMongoPort" >>$mongoConfig
+        fi
+        echo "nohttpinterface=true" >>$mongoConfig
+        echo "replSet = rs0" >>$mongoConfig
+        chmod 644 /etc/$mongoConfig
+
     fi
-    echo "nohttpinterface=true" >>$mongoConfig
-    echo "replSet = rs0" >>$mongoConfig
-    cp $mongoConfig /etc/
-    chmod 644 /etc/$mongoConfig
 
 }
 
-function InstallMongoService(){
-
-    echo "Installing MongoServer config file..........."
+function installMongoService(){
+    echo "installing mongoserver"
     if [ ! -f "/usr/bin/mongo" ]
     then
-    	tar zxvf ./mongodb-linux-x86_64-2.4.9.tgz
-    	cd ./mongodb-linux-x86_64-2.4.9/bin/
-    	cp * /usr/bin
-    	cd ../..
+        tar zxvf ./mongodb-linux-x86_64-2.4.9.tgz
+        cd ./mongodb-linux-x86_64-2.4.9/bin/
+        cp * /usr/bin
+        cd ../.
     else
-    	echo "Mongo server file already exsit"
+        echo "mongo server file already exist"
     fi
+
     cp mongod-$1.sh /etc/init.d/
     chmod 755 /etc/init.d/mongod-$1.sh
-    /etc/init.d/mongod-$1.sh start 
-
+    /etc/init.d/mongod-$1.sh start
     i=1
     until ((i=="0"))
-    do 
-    	/bin/cat /var/log/mongodb-$1/mongodb.log | grep "waiting for connection on port"
-    	i=$?
-    	sleep 3
-    	echo "Waiting for mongodb ready"
+    do
+        /bin/cat /var/log/mongodb-$1/mongodb.log | grep "waiting for connection"
+        i=$?
+        sleep 3
+        echo "waiting for mongodb ready"
     done
-    echo "Mongodb is ready"
+    echo "mongodb is ready"
     sleep 4
-
 }
 
-function SetupReplicationSet(){
+function setupReplSet(){
 
-    echo "Setup MongoDB ReplicationSet ......."
+    echo "setup mongodb replicationset"
     case $1 in
         master|Master|MASTER)
-			member=("$SlaveName:$SlaveMongoPort")
-			echo "rs.initiate()" | /usr/bin/mongo $MasterName:$MasterMongoPort
-			sleep 3
-			for count in ${member[@]}
-			do
-				echo "rs.add(\"$count\")"|/usr/bin/mongo $MasterName:$MasterMongoPort
-				sleep 3
-			done
-			echo "rs.addArb(\"$ArbiterName:$ArbiterMongoPort\")"|/usr/bin/mongo $MasterName:$MasterMongoPort    
-			sleep 3
-			echo "rs.status()"|/usr/bin/mongo $MasterName:$MasterMongoPort
-    ;;
-    slave|Slave|SLAVE)
-	exit 0
-	;;
-	arbiter|Arbiter|ARBITER)
-	exit 0
-	;;
-	*)
-	exit 0
-	;;		
-    esac
-
+            member=("$SlaveName:$SlaveMongoPort")
+            echo "rs.initiate()" | /usr/bin/mongo $MasterName:$MasterMongoPort
+            sleep 3
+            for count in ${member[@]}
+            do
+                echo "rs.add(\"$count\")" | /usr/bin/mongo $MasterName:$MasterMongoPort
+                sleep 3
+            done
+            echo "rs.addArb(\"$ArbiterName:$ArbiterMongoPort\")"|/usr/bin/mongo $MasterName:$MasterMongoPort
+            echo "rs.status()"|/usr/bin/mongo $MasterName:$MasterMongoPort
+        ;;
+        slave|Slave|SLAVE)
+        exit 0
+        ;;
+        arbiter|Arbiter|ARBITER)
+        exit 0
+        ;;
+        *)
+        exit 0
+        ;;
 }
+
+
 
 case $1 in
     master|Master|MASTER)
