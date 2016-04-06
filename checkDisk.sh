3492a5abf9d1b2f62edb03f01d039095a29c2935
Shanker <shankerlee@gmail.com>
2016-04-06 18:03:45 +0800

    update

--- checkDisk.sh
+++ checkDisk.sh
@@ -6,27 +6,28 @@ date=$(date +"%Y%m%d")
 servers="./server_list"
 
 function getResult(){
-if [ ! -f disk.log.$date ]
-then
-    echo "=======Geting the log...========="
-    for i in `cat $servers`
-    do
-         echo $i >>disk.log.$date
-        ssh $i "/cygdrive/c/CGuardian/tools/MegaCli.exe -cfgdsply-aALL|grep -E -i 'err|fail'" >> disk.log.$date
-    done
-fi
+
+    if [ ! -f disk.log.$date ]
+    then
+        echo "=======Geting the log...========="
+        for i in `cat $servers`
+        do
+            echo $i >>disk.log.$date
+            ssh $i "/cygdrive/c/CGuardian/tools/MegaCli.exe -cfgdsply-aALL|grep -E -i 'err|fail'" >> disk.log.$date
+        done
+    fi
 }
 
 
 function showFailure(){
 
-echo -e "Predictive Failure Count is:\n"
+    echo -e "Predictive Failure Count is:\n"
 
-cat disk.log.$date | awk '/Predictive Failure Count/ {s +=$NF} END { if(s>0) print s}'
+    cat disk.log.$date | awk '/Predictive Failure Count/ {s +=$NF} END { if(s>0) print s}'
 
-echo -e "Media Error Count is : \n"
+    echo -e "Media Error Count is : \n"
 
-cat disk.log.$date | awk '/Media Error Count/ {m +=$NF} END { if(m<1000) print m}'
+    cat disk.log.$date | awk '/Media Error Count/ {m +=$NF} END { if(m<1000) print m}'
 
 }
 
