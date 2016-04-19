c2accb3df530e8574cdab9e067c6b4fad5a54e71
sangrealest <shankerlee@gmail.com>
2016-04-19 05:52:46 +0000

    update

--- NIC_usage.sh
+++ NIC_usage.sh
@@ -11,14 +11,14 @@ fi
 IF=$1
 while true
 do
-     R1=`cat /sys/class/net/$1/statistics/rx_bytes`
-     T1=`cat /sys/class/net/$1/statistics/tx_bytes`
+     R1=$(cat /sys/class/net/$1/statistics/rx_bytes)
+     T1=$(cat /sys/class/net/$1/statistics/tx_bytes)
      sleep $INTERVAL
-     R2=`cat /sys/class/net/$1/statistics/rx_bytes`
-     T2=`cat /sys/class/net/$1/statistics/tx_bytes`
-     TBPS=`expr $T2 - $T1`
-     RBPS=`expr $R2 - $R1`
-     TKBPS=`expr $TBPS / 1024`
-     RKBPS=`expr $RBPS / 1024`
+     R2=$(cat /sys/class/net/$1/statistics/rx_bytes)
+     T2=$(cat /sys/class/net/$1/statistics/tx_bytes)
+     TKBPS=$(expr $(expr $T2 - $T1) / 1024)
+     RKBPS=$(expr $(expr $R2 - $R1) / 1024)
+#     TKBPS=$(expr $TBPS / 1024)
+#     RKBPS=$(expr $RBPS / 1024)
      echo "TX $1: $TKBPS KB/s RX $1: $RKBPS KB/s"
 done
