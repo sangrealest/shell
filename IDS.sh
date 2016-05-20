81f09fb8966dcbaff6abe6909563cd31a5f29446
Shanker <shankerlee@gmail.com>
2016-05-20 14:47:54 +0800

    update

--- IDS.sh
+++ IDS.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #intruder_detect
 #detect the invlaid users
+
 AUTHLOG=/var/log/syslog
 if [ -n "$1" ]
 then
-  AUTHLOG=$1
+    AUTHLOG=$1
     echo "Using log file:$AUTHLOG"
     fi
     LOG=/tmp/valid.$$.log
@@ -18,23 +19,23 @@ then
       grep $ip $LOG > /tmp/temp.$$.log
         for user in $users
           do
-              grep $user /tmp/temp.$$.log > /tmp/$$.log
-                  cut -c-16 /tmp/$$.log > /tmp/$$.time
-                      tstart=$(head -1 /tmp/$$.time)
-        start=$(date -d "$tstart" "+%s")
+            grep $user /tmp/temp.$$.log > /tmp/$$.log
+            cut -c-16 /tmp/$$.log > /tmp/$$.time
+            tstart=$(head -1 /tmp/$$.time)
+            start=$(date -d "$tstart" "+%s")
             tend=$(tail -1 /tmp/$$.time)
-        end=$(date -d "$tend" "+%s")
+            end=$(date -d "$tend" "+%s")
             limit=$(($end - $start))
-        if [ $limit -gt 20 ]
+            if [ $limit -gt 20 ]
             then
-                  let ucount++
-                        IP=$(egrep -o  "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" /tmp/$$.log|head -1)
-                              TIME_RANGE="$tstart--->$tend"
-                                    ATTEMPTS=$(cat /tmp/$$.log|wc -l)
-          HOST=$(host $IP |awk '{print $NF}')
+                let ucount++
+                IP=$(egrep -o  "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" /tmp/$$.log|head -1)
+                TIME_RANGE="$tstart--->$tend"
+                ATTEMPTS=$(cat /tmp/$$.log|wc -l)
+                HOST=$(host $IP |awk '{print $NF}')
                 printf "%-5s|%-10s|%-10s|%-13s|%-33s|%s\n" "$ucount" "$user" "$ATTEMPTS" "$IP" "$HOST" "$TIME_RANGE"
-                    fi
-                        done
-                        done
-                        rm /tmp/valid.$$.log /tmp/$$.log /tmp/$$.time /tmp/temp.$$.log 2>/dev/null
+            fi
+         done
+    done
+rm /tmp/valid.$$.log /tmp/$$.log /tmp/$$.time /tmp/temp.$$.log 2>/dev/null
                               
