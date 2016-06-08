b867ad832b28de9727b6da8c7d9134ca3c2fbaa8
Shanker <shanker@yeah.net>
2016-06-08 07:46:56 +0000

    update

---
+++ find_cache.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#Author: Shanker
+#Time: 2016/06/08
+
+#set -e
+#set -u
+#you have to install linux-fincore
+if [ ! -f /usr/local/bin/linux-fincore ]
+then
+    echo "You haven't installed linux-fincore yet"
+    exit
+fi
+
+#find the top 10 processs' cache file
+ps -e -o pid,rss|sort -nk2 -r|head -10 |awk '{print $1}'>/tmp/cache.pids
+#find all the processs' cache file
+#ps -e -o pid>/tmp/cache.pids
+
+if [ -f /tmp/cache.files ]
+then
+    echo "the cache.files is exist, removing now "
+    rm -f /tmp/cache.files
+fi
+
+while read line
+do
+    lsof -p $line|awk '{print $9}' >>/tmp/cache.files 
+done</tmp/cache.pids
+
+
+if [ -f /tmp/cache.fincore ]
+then
+    echo "the cache.fincore is exist, removing now"
+
+    rm -f /tmp/cache.fincore
+fi
+
+for i in `cat /tmp/cache.files`
+do
+
+    if [ -f $i ]
+    then
+
+        echo $i >>/tmp/cache.fincore
+    fi
+done
+
+linux-fincore -s  `cat /tmp/cache.fincore`
+
+rm -f /tmp/cache.{pids,files,fincore}
