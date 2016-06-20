4c9090c5c04606685fbb8687482b441e8d30d55b
Shanker <shanker@yeah.net>
2016-06-20 10:03:40 +0000

    update

--- iptables_filter_badssh.sh
+++ iptables_filter_badssh.sh
@@ -6,6 +6,22 @@
 #If some try to contect your server 4 times within 60 seconds, then drop the session.
 
 port=22
+inet=eth1
+echo "$0 -p ssh port(default is 22)  -i internet face(default is eth1)"
+while getopts ":p:i:" opt
+do
+    case "$opt" in
+    "p")
+        port=$OPTARG
+    ;;
+    "i")
+        inet=$OPTARG
+    ;;
+    *)
+        echo "unknown args"
+    ;;
+    esac
+done
 
-iptables -I INPUT -p tcp --dport $port -i eth0 -m state --state NEW -m recent --set
-iptables -I INPUT -p tcp --dport $port -i eth0 -m state --state NEW -m recent --update --seconds 60 --hitcount 4 -j DROP
+iptables -I INPUT -p tcp --dport $port -i $inet -m state --state NEW -m recent --set
+iptables -I INPUT -p tcp --dport $port -i $inet -m state --state NEW -m recent --update --seconds 60 --hitcount 4 -j DROP
