58615b5d53cd8bc3eda060b50d701fa2631b488d
root <root@bastion.shanker>
2016-09-29 10:40:44 +0800

    update

---
+++ RSS.sh
@@ -0,0 +1,12 @@
+#/bin/bash                                                                                                              
+#From Yufeng's Blog
+for PROC in `ls  /proc/|grep "^[0-9]"`
+do
+  if [ -f /proc/$PROC/statm ]; then
+      TEP=`cat /proc/$PROC/statm | awk '{print ($2)}'`
+      RSS=`expr $RSS + $TEP`
+  fi
+done
+RSS=`expr $RSS \* 4`
+echo $RSS"KB"
+
