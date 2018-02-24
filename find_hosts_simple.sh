c3d34d8e7d2fbce03afb2f42601475b7b5f609a6
root <root@bastion.shanker>
2018-02-24 14:05:24 +0800

    find_hosts_simple tuned

--- find_hosts_simple.sh
+++ find_hosts_simple.sh
@@ -2,15 +2,16 @@
 #author:shanker
 #date:2012/6/21
 set -u
-HOST="192.168.196."
+HOST="192.168.122."
 trap "exit" 2 3
-for i in $(seq 1 25)
+for i in $(seq 1 255)
     do
-    ping -c 1 -W 1 "$HOST$i" &>/dev/null
+(    ping -c 1 -W 1 "$HOST$i" &>/dev/null
     if [ $? -eq 0 ]
     then
     echo ""$HOST$i" is alive"
     else
     echo ""$HOST$i" is down"
     fi
+)&
     done
